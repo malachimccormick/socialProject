e7199403bc33ce5e37771d425b2d3f001c2c1cb0
Malachi MCCormick <MalachiMccormick@gmail.com>
2019-03-14 13:08:38 -0400

    adding to routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+//Load Validation
+const validateProfileInput = require('../../valadation/profile')
+
 //Load profile model
 const Profile = require('../../models/Profile')
 
@@ -26,6 +29,7 @@ router.get('/', passport.authenticate('jwt', {
     Profile.findOne({
             user: req.user.id
         })
+        .populate('user', ['name', 'avatar'])
         .then(profile => {
             if (!profile) {
                 errors.noprofile = 'There is no profile for this user'
@@ -36,12 +40,94 @@ router.get('/', passport.authenticate('jwt', {
         .catch(err => res.status(404).json(err));
 });
 
+//@route    Get api/profile/all
+//@desc     Get all profiles
+//@access   Public
+
+router.get('/all', (req, res) => {
+    const errors = {}
+
+    Profile.find()
+        .populate('user', ['name', 'avatar'])
+        .then(profiles => {
+            if (!profiles) {
+                errors.noprofile = 'There are no profiles'
+                return res.status(404).json(errors)
+            }
+            res.json(profiles)
+        })
+        .catch(err => res.status(404).json({
+            profile: 'There are no profiles'
+        }))
+})
+
+
+//@route    Get api/profile/handle/:handle
+//@desc     Get profile by handle
+//@access   Public
+
+router.get('/handle/:handle', (req, res) => {
+
+    const errors = {}
+
+    Profile.findOne({
+            handle: req.params.handle
+        })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                res.Status(404).json(errors);
+            }
+            res.json(profile)
+        })
+        .catch(err => res.status(404).json({
+            noprofile: 'There is no profile for this user'
+        }))
+});
+
+//@route    Get api/profile/user/:user_id
+//@desc     Get profile by user ID
+//@access   Public
+
+router.get('/user/:user_id', (req, res) => {
+
+    const errors = {}
+
+    Profile.findOne({
+            user: req.params.user_id
+        })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                res.Status(404).json(errors);
+            }
+            res.json(profile)
+        })
+        .catch(err => res.status(404).json({
+            noprofile: 'There is no profile for this user'
+        }))
+})
+
 //@route    Post api/profile
 //@desc     Create/update user profile
 //@access   Private
 router.post('/', passport.authenticate('jwt', {
     session: false
 }), (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateProfileInput(req.body)
+
+    //Check Validation
+    if (!isValid) {
+        //Return any errors with 400 status
+        return res.status(400).json(errors)
+    }
+
+
     // get fields
     const profileFields = {};
     profileFields.user = req.user.id;
@@ -98,6 +184,31 @@ router.post('/', passport.authenticate('jwt', {
             }
         })
 
+});
+
+//@route    Post api/profile/experience
+//@desc     Add experience to profile
+//@access   Private
+router.post('/experience', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    Profile.findOne({
+            user: req.user.id
+        })
+        .then(profile => {
+            const newExp = {
+                title: req.body.title,
+                company: req.body.company,
+                location: req.body.from,
+                from: req.body.from,
+                to: req.body.to,
+                current: req.body.current,
+                description: req.body.description
+            }
+            //Add to experience array
+            profile.experience.unshift(newExp);
+            profile.save().then(profile => res.json(profile))
+        })
 })
 
 module.exports = router;
