ac3a640170574f17206f4d2fa4b6741ca8a89260
Malachi MCCormick <MalachiMccormick@gmail.com>
2019-03-20 22:20:41 -0400

    back on track

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // Register Use
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +15,38 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login - Get User Token
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      //Save to local storage
+      const { token } = res.data;
+
+      // Set token to local storage
+      localStorage.setItem("jwtToken", token);
+      // Set token to auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      //Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,6 @@
+import { SET_CURRENT_USER } from "../actions/types";
+import isEmpty from "../validation/is-empty";
+
 const intialState = {
   isAuthenticated: false,
   user: {}
@@ -5,6 +8,12 @@ const intialState = {
 
 export default function(state = intialState, action) {
   switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
     default:
       return state;
   }
