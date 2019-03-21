0eaf62d9a32dcc61557bc4096722ba6d18d88ddb
Malachi MCCormick <MalachiMccormick@gmail.com>
2019-03-20 23:21:55 -0400

    login bar corrected

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import store from "./store";
 import Navbar from "./components/layout/Navbar";
@@ -22,6 +22,16 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+  //Check for expired Token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    //Logout user
+    store.dispatch(logoutUser());
+    //Clear Current Profile
+
+    //Redirect to login
+    window.location.href = "/login";
+  }
 }
 
 class App extends Component {
