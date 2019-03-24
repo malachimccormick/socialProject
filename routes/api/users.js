f6d786ed892a4682e4903e39095f12378ee7b609
Malachi MCCormick <MalachiMccormick@gmail.com>
2019-03-24 01:58:47 -0400

    test3

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys_prod");
+const keys = require("../../config/keys");
 const passport = require("passport");
 
 //Load input Validation
